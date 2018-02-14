701ab3989e994dcb430f8a22c01ad1594a93fe55
Job <1769393445@qq.com>
2018-02-14 23:13:15 +0800

    写入调用类的代码，并将”canglaoshi“作为参数传入

--- LearnClass/Person.py
+++ LearnClass/Person.py
@@ -7,7 +7,7 @@ class Person:
     def __init__(self, name):
         self.name = name
 
-    def get_neme(self):
+    def get_name(self):
         return self.name
 
     def color(self, color):
@@ -15,3 +15,11 @@ class Person:
         d[self.name] = color
         return d
 
+if __name__ == "__main__":
+    girl = Person("canglaoshi")
+    print(girl.name)
+    name = girl.get_name()
+    print(name)
+    her_color = girl.color("white")
+    print(her_color)
+    
