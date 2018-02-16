b5af88fb4ce4099b516aaedd9bfb1ae87f80acd5
Job <1769393445@qq.com>
2018-02-17 01:00:45 +0800

    添加判断成绩等级的方法

--- LearnClass/ClassLiao/ClassStudent.py
+++ LearnClass/ClassLiao/ClassStudent.py
@@ -9,8 +9,17 @@ class Student(object):
     def print_score(self):
         print('%s: %s' %(self.name, self.score))
 
+    def get_grade(self):
+        if self.score >= 90:
+            return 'A'
+        elif self.score >= 60:
+            return 'B'
+        else:
+            return 'C'
+
 bart = Student('Bart Simpson', 59)
 lisa = Student('Lisa Simpson', 87)
 bart.print_score()
 lisa.print_score()
-
+print(bart.name, bart.get_grade())
+print(lisa.name, lisa.get_grade())
